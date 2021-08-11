24d98587f6bc7e2e86fda53c65ac3c496ab61ecb
gktejus <gktejus@gmail.com>
2021-08-11 22:02:18 +0530

    fixes

--- models/models.py
+++ models/models.py
@@ -1,11 +1,9 @@
-from .birealnet import *
-from .irnet import * 
+from .birealnet import birealnet18
+from .irnet import get_irnet 
 
 def get_model(model, num_classes , insize):
     if model=="r18_bireal":
         return birealnet18(num_classes)
     else:
         return get_irnet(num_classes)
-    
-
 
